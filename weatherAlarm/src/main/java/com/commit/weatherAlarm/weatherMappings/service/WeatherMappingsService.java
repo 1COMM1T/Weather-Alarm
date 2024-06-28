829101506ff3115dc4d0bb363e928c1bcf6ca86a
dongh810 <105986200+dongh810@users.noreply.github.com>
2024-06-29 04:03:00 +0900

    Feat: 키값 조회 서비스 추가

--- weatherAlarm/src/main/java/com/commit/weatherAlarm/weatherMappings/service/WeatherMappingsService.java
+++ weatherAlarm/src/main/java/com/commit/weatherAlarm/weatherMappings/service/WeatherMappingsService.java
@@ -1,4 +1,7 @@
 package com.commit.weatherAlarm.weatherMappings.service;
 
+import com.commit.weatherAlarm.weatherMappings.view.KeyView;
+
 public interface WeatherMappingsService {
+    KeyView searchFileByEmail(String email);
 }
