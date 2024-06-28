b24b5392901b8a3b2b8d12b4d10fd02dba8250f4
dongh810 <105986200+dongh810@users.noreply.github.com>
2024-06-29 04:02:33 +0900

    Feat: 키값 조회기능 추가

--- weatherAlarm/src/main/java/com/commit/weatherAlarm/weatherMappings/controller/WeatherMappingsController.java
+++ weatherAlarm/src/main/java/com/commit/weatherAlarm/weatherMappings/controller/WeatherMappingsController.java
@@ -1,9 +1,16 @@
 package com.commit.weatherAlarm.weatherMappings.controller;
 
 import com.commit.weatherAlarm.weatherMappings.service.WeatherMappingsService;
+import com.commit.weatherAlarm.weatherMappings.view.KeyView;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/v1/weather-mappings")
@@ -14,5 +21,12 @@ public class WeatherMappingsController {
     @Autowired
     public WeatherMappingsController(WeatherMappingsService weatherMappingsService) {
         this.weatherMappingsService = weatherMappingsService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<KeyView> searchFileByEmail(@RequestParam(value = "email", required = true) String email) {
+        KeyView result = weatherMappingsService.searchFileByEmail(email);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+
     }
 }
