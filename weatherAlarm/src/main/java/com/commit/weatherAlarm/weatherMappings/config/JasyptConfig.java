c5151b098d8687cc2be5dd2ff6e05573bb63574a
bucky1005 <null00007@naver.com>
2024-06-29 16:26:36 +0900

    Init: ec2 연동용 환경 설정
    jasyptConfig 추가 및 yml 파일 수정

---
+++ weatherAlarm/src/main/java/com/commit/weatherAlarm/weatherMappings/config/JasyptConfig.java
@@ -0,0 +1,41 @@
+package com.commit.weatherAlarm.weatherMappings.config;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEncryptableProperties
+public class JasyptConfig {
+
+    @Value("${jasypt.encryptor.password}")
+    private String password;
+
+    @Value("${jasypt.encryptor.poolSize:1}")  // 기본값을 1로 설정
+    private String poolSize;
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(password);
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+
+        // 풀 사이즈 설정
+        config.setPoolSize(poolSize);
+
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator");
+        config.setStringOutputType("base64");
+
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}
+
