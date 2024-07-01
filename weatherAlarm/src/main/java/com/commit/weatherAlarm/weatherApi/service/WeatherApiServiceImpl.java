02d34273162b408237dd145daa7ecc28683fb925
dongh810 <105986200+dongh810@users.noreply.github.com>
2024-07-01 14:06:26 +0900

    Fix: 날씨정보를 한글로 받아올 수 있도록 변경

--- weatherAlarm/src/main/java/com/commit/weatherAlarm/weatherApi/service/WeatherApiServiceImpl.java
+++ weatherAlarm/src/main/java/com/commit/weatherAlarm/weatherApi/service/WeatherApiServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Service
-public class WeatherApiServiceImpl implements WeatherApiService{
+public class WeatherApiServiceImpl implements WeatherApiService {
 
     private final String apiKey = "1b63b6cc88cd0a3b9c0b68934ddca166";
     private final WebClient webClient;
@@ -23,6 +23,7 @@ public class WeatherApiServiceImpl implements WeatherApiService{
                         .queryParam("id", cityCode)
                         .queryParam("appid", apiKey)
                         .queryParam("units", "metric") // 온도를 섭씨로 받기 위해 units 파라미터 추가
+                        .queryParam("lang", "kr")
                         .build())
                 .retrieve()
                 .bodyToMono(String.class)
@@ -34,20 +35,19 @@ public class WeatherApiServiceImpl implements WeatherApiService{
                         JsonNode weatherNode = root.path("weather").get(0);
                         JsonNode mainNode = root.path("main");
 
-                        String main = weatherNode.path("main").asText();
+                        String description = weatherNode.path("description").asText(); // 날씨 설명
                         String icon = weatherNode.path("icon").asText();
                         double temp = mainNode.path("temp").asDouble();
                         String cityName = root.path("name").asText();
 
                         String iconUrl = String.format("http://openweathermap.org/img/wn/%s.png", icon);
 
-                        return String.format("Weather: %s, Location: %s, Temp: %.2f°C, Icon: %s",
-                                main, cityName, temp, iconUrl);
+                        return String.format("날씨: %s, 위치: %s, 온도: %.2f°C, 아이콘: %s",
+                                description, cityName, temp, iconUrl);
                     } catch (Exception e) {
                         e.printStackTrace();
-                        return "Error parsing weather data";
+                        return "날씨 데이터를 파싱하는 중 오류가 발생했습니다.";
                     }
                 });
     }
 }
-
